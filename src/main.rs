ae557b9d5f39ef53ea892cba05df72a0f33e9293
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-04-28 23:42:13 +0100

    get hash from text [add a dedicated fn]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use sha2::{Digest, Sha256};
 
+fn hash_from_text(text: &str) -> String {
+    let hash = Sha256::digest(text.as_bytes());
+    format!("{:x}", hash)
+}
 fn main() {
     let text = "Paolo";
-    let hash = Sha256::digest(text.as_bytes());
-    println!("SHA256 hash of '{}' is {:x}", text, hash);
+    println!("SHA256 hash of '{}' is {}", text, hash_from_text(text));
 }
