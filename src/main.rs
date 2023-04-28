771f7bf1a6e3a66f64aee652ec0b96ef8a41e521
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-04-28 23:29:17 +0100

    first result okay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use sha2::{Digest, Sha256};
+
 fn main() {
-    println!("Hello, world!");
+    let text = "Paolo";
+    let hash = Sha256::digest(text.as_bytes());
+    println!("SHA256 hash of '{}' is {:x}", text, hash);
 }
